bd10d035b0315fde94f8461144aefd7e9f7e31bf
Collinveerman1 <collinveerman1@gmail.com>
2021-09-23 12:13:57 +0200

    eerste commit

---
+++ PYTB1L2FamliyTies/familyties.py
@@ -0,0 +1,8 @@
+line1 = "Ik ben Collin."
+line2 = "Ik ben 16 jaar oud."
+line3 = "Ik kom uit Volendam."
+line4 = "Ik woon 27,6km van Mediacollege af."
+line5 = "Ik vind school tot nu toe heel leuk!"
+
+print(line1 + line2 + line3)
+print(line4 + line5)
