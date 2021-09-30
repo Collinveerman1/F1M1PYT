a0f863fd9e80c08f7e15a39cb4007261785990b7
Collinveerman1 <collinveerman1@gmail.com>
2021-09-30 12:56:52 +0200

    eerste commit

---
+++ PYTB1L3DailyChoices/DailyChoices.py
@@ -0,0 +1,66 @@
+print("Je word wakker, blijf je liggen of sta je op?")
+print("A, Opstaan")
+print("B, Blijven liggen")
+
+scriptinput = input()
+if scriptinput == "a" or scriptinput == "A":
+    scrip = True
+    print("Je gaat uit bed en voelt jezelf uitgerust.")
+    print("")
+elif scriptinput == "B" or scriptinput == "b":
+    print("Je blijft nog even liggen en gaat daarna uit bed.")
+    print("")
+
+print("Tijd voor ontbijt, ga je een eitje bakken of tosti's maken?")
+print("A, Eitje bakken")
+print("B, Tosti's maken")
+
+scriptinput = input()
+if scriptinput == "a" or scriptinput == "A":
+    scrip = True
+    print("Aah eitje bakken, lekker!")
+    print("")
+elif scriptinput == "B" or scriptinput == "b":
+    print("Aah tosti's maken, lekker!")
+    print("")
+
+print("Wat ga je aan doen vandaag?")
+print("A, Een chill outfit")
+print("B, Jeans met een t shirt")
+
+scriptinput = input()
+if scriptinput == "a" or scriptinput == "A":
+    scrip = True
+    print("Lekker warm!")
+    print("")
+elif scriptinput == "b" or scriptinput == "B":
+    print("Coole outfit!")
+    print("")
+
+
+print("Hoe ga je nar school?")
+print("A, Bus 316 + bus 22")
+print("B, Bus 110 + sprinter")
+
+scriptinput = input()
+if scriptinput == "a" or scriptinput == "A":
+    scrip = True
+    print("Goeie keuze!")
+    print("")
+elif scriptinput == "b" or scriptinput == "B":
+    print("Lang onderweg maar doe wat je wilt.")
+    print("")
+
+
+print("Ga je vooraan zitten of achteraan?")
+print("A, Vooraan")
+print("B, Achteraan")
+
+scriptinput = input()
+if scriptinput == "a" or scriptinput == "A":
+    scrip = True
+    print("Opletten is niet echt je ding maar ga er voor")
+    print("")
+elif scriptinput == "b" or scriptinput == "B":
+    print("Nice, lekker praten met de boys!")
+    print("")
