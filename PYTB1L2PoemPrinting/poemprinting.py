87a9b256f2e1153bc9078ab77d0e37f1b018e208
Collinveerman1 <collinveerman1@gmail.com>
2021-11-11 11:18:03 +0100

    eerste commit

---
+++ PYTB1L2PoemPrinting/poemprinting.py
@@ -0,0 +1,6 @@
+if True:
+ vers1 = "Wil een hele dure tas. En een hele zware pas. Weet dat je al op me was. Je weet dat ik je verras."
+ vers2 = "Kom met film maar geen minion. Ik wil het liefst een paar million. Doe me best ja daarvoor ren ik hard. Opgegroeid zonder hart in de 21 met Bart."
+ vers3 = "Ben van de twentyone geen Savage. Je komt hard maar maakt geen damage. Met donna Game Cube doe je hayek op de stoep. Jouw pokoe dat is troep je wil doezoe op je voet"
+
+print (vers1,vers2, vers3)
